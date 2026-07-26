0d6ac3b0f22a4ab8dde908f93a007dcf6334640d
abinash-shrestha119 <abinash.shrestha119@gmail.com>
2026-07-26 12:09:38 +0545

    Fix authentication cookies for productions

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://nutriplus-eight.vercel.app/",
+    origin: "https://nutriplus-eight.vercel.app",
     credentials: true,
   })
 );
