1ddd8c7f608bb68735d1763f301b4d9b37d28606
abinash-shrestha119 <abinash.shrestha119@gmail.com>
2026-07-28 12:54:29 +0545

    admin fixed

--- frontend/src/screens/orderScreen/orderScreen.jsx
+++ frontend/src/screens/orderScreen/orderScreen.jsx
@@ -172,14 +172,6 @@ const OrderScreen = () => {
                 </ListGroup.Item>
               )}
 
-              {!order.isPaid && !canPay && userInfo?.isAdmin && (
-                <ListGroup.Item className="list-item">
-                  <Message variant="info">
-                    {/* Admins can view this order but cannot pay it. */}
-                  </Message>
-                </ListGroup.Item>
-              )}
-
               {loadingDeliver && <Loader />}
 
               {userInfo &&
